ed9fc362e0c898604d2efd80a3e27e410333ebca
jpierrot <je.pierrot@gmail.com>
2023-03-14 14:23:29 +0100

    dev/mproducts: mod Category and Product pojo (id GenerationType was wrong : now set to IDENTIFIER instead of AUTO)

--- ventalis_mproducts/src/main/java/com/jpierrot/ventalismproducts/pojo/Category.java
+++ ventalis_mproducts/src/main/java/com/jpierrot/ventalismproducts/pojo/Category.java
@@ -12,7 +12,7 @@ import lombok.*;
 @Table(name = "categories")
 public class Category {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
--- ventalis_mproducts/src/main/java/com/jpierrot/ventalismproducts/pojo/Product.java
+++ ventalis_mproducts/src/main/java/com/jpierrot/ventalismproducts/pojo/Product.java
@@ -13,7 +13,7 @@ import lombok.*;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
