ac80ecf3546d70ddd28838a2bbd160818c698fd3
jpierrot <je.pierrot@gmail.com>
2023-03-17 20:41:44 +0100

    mmail : initial commit, add module to IntelliJ project

---
+++ ventalis_mmail/src/test/java/com/jpierrot/ventalismmail/VentalisMmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpierrot.ventalismmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentalisMmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
