4262dd70d22349af084fc07a7d092b95a82678e9
jpierrot <je.pierrot@gmail.com>
2023-03-19 19:41:27 +0100

    dev/security: initial commit (module 'msecurity' added to the project), MariaDB

---
+++ ventalismsecurity/src/main/java/com/jpierrot/ventalismsecurity/VentalismsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.jpierrot.ventalismsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentalismsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentalismsecurityApplication.class, args);
+	}
+
+}
