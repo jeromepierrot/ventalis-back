bcece826485ec3c7fa5a131ce14bcd0f7fb6c408
jpierrot <je.pierrot@gmail.com>
2023-03-19 15:49:48 +0100

    mmail : (in progress) major changes using 'SimpleMailMessage' and 'JavaMailSender' classes from Spring boot javamail framework (not tested yet)

---
+++ ventalis_mmail/src/main/java/com/jpierrot/ventalismmail/services/impl/EmailServiceImpl.java
@@ -0,0 +1,23 @@
+package com.jpierrot.ventalismmail.services.impl;
+
+import com.jpierrot.ventalismmail.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailServiceImpl implements EmailService {
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendSimpleMessage(
+        String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@ventalis.fr");
+        message.setFrom(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+}
